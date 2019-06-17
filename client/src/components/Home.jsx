ccba91756e84feca3d8f994081a209034ce94268
lajiang2 <lawrencejiang@berkeley.edu>
2019-06-17 17:57:29 +0300

    connected to database, made dropdowns

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-import { LineChart } from 'react-chartkick'
+import Dropdown from 'react-dropdown';
+import Select from 'react-select';
+import { PieChart, LineChart } from 'react-chartkick'
 import 'chart.js'
 import './Home.css';
+import 'react-dropdown/style.css';
+
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       broadcastReachData: '',
-      ebookData: {},
+      ebooksData: [],
+      iOSData: [],
+      albumsData: [],
+      selectedYear: '',
+      selectedMonth: ''
     };
+    this._onYearSelect = this._onYearSelect.bind(this);
+    this._onMonthSelect = this._onMonthSelect.bind(this);
   }
 
   componentDidMount() {
     this.updateBroadcastReach();
-    this.updateEbookData();
+    this.updateEbooksData();
+    this.updateiOSData();
+    this.updateAlbumsData();
   }
 
   async updateBroadcastReach() {
@@ -31,34 +43,94 @@ class Home extends Component {
     });
   }
 
-  async updateEbookData() {
-    const res = await fetch('/api/ebook');
+  async updateEbooksData() {
+    const res = await fetch('/api/revenue/ebooks');
     const resJson = res.json();
     resJson.then(result => {
-      // let i;
-      // for (i = 0; i < result.length; i++) { 
-      //   if (typeof result[i][1] !== 'undefined') {
-      //     result[i][1] = parseInt(result[i][1].replace(/,/g, ''), 10)
-      //   }
-      // }
-      this.setState({ ebookData: result });
+      // var ebooksArr = {};
+      var ebooksArr = [];
+      let i;
+      for (i = 0; i < result.length; i++) { 
+        // ebooksArr[result[i].month + result[i].year] = result[i].revenue;
+        ebooksArr.push([result[i].month + result[i].year, result[i].revenue]);
+      }
+      this.setState({ ebooksData: ebooksArr });
     });
   }
 
+  async updateiOSData() {
+    const res = await fetch('/api/revenue/ios');
+    const resJson = res.json();
+    resJson.then(result => {
+      // var ebooksArr = {};
+      var iOSArr = [];
+      let i;
+      for (i = 0; i < result.length; i++) { 
+        // ebooksArr[result[i].month + result[i].year] = result[i].revenue;
+        iOSArr.push([result[i].month + result[i].year, result[i].revenue]);
+      }
+      this.setState({ iOSData: iOSArr });
+    });
+  }
+
+  async updateAlbumsData() {
+    const res = await fetch('/api/revenue/albums');
+    const resJson = res.json();
+    resJson.then(result => {
+      // var ebooksArr = {};
+      var albumsArr = [];
+      let i;
+      for (i = 0; i < result.length; i++) { 
+        // ebooksArr[result[i].month + result[i].year] = result[i].revenue;
+        albumsArr.push([result[i].month + result[i].year, result[i].revenue]);
+      }
+      this.setState({ albumsData: albumsArr });
+    });
+  }
+
+  _onYearSelect(option) {
+    this.setState({ selectedYear: option });
+  }
+
+  _onMonthSelect(option) {
+    this.setState({ selectedMonth: option });
+  }
+
   render() {
+    const defaultYear = this.state.selectedYear;
+    const defaultMonth = this.state.selectedMonth;
+    const years = ["2018", "2019"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     return (
       <div className="home">
-        
           <h1 className="home-title"> hello there! </h1>
           <h3 className="home-subtitle"> welcome to the ubongo data dashboard :) </h3>
+          <div className="dropdown">
+            <Dropdown
+              className=""
+              options={years}
+              onChange={this._onYearSelect}
+              value={defaultYear}
+              placeholder="Select a year"
+            />
+          </div>
+          <div className="dropdown">
+            <Dropdown
+              className=""
+              options={months}
+              onChange={this._onMonthSelect}
+              value={defaultMonth}
+              placeholder="Select a month"
+            />
+          </div>
           <div className="container">
             <div className="box">
               <div className="title"> Broadcast Reach Tracker </div>
               <LineChart data={this.state.broadcastReachData} xtitle="Month" ytitle="Reach (in millions)" colors={["#83D3CF", "red"]}/>
             </div>
             <div className="box">
-              <div className="title"> Ebook Revenue </div>
-              <LineChart data={this.state.ebookData} xtitle="Month" ytitle="Revenue (in dollars)" colors={["#83D3CF", "red"]}/>
+              <div className="title"> ebooks Revenue </div>
+              <PieChart data={this.state.ebooksData} xtitle="Month" ytitle="Revenue (in dollars)"/>
             </div>
           </div>
         </div>
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const Router = require('express-promise-router');
+require('dotenv').config()
 const path = require('path');
-var bodyParser = require('body-parser');
-const app = express();
 const fs = require('fs');
+const express = require('express');
 const readline = require('readline');
+const app = express();
+const bodyParser = require('body-parser');
 const {google} = require('googleapis');
+const db = require('./db/database');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+// auth variable
 var auth;
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
@@ -47,63 +49,6 @@ function authorize(credentials) {
   });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-// function getNewToken(oAuth2Client, callback) {
-function getNewToken(oAuth2Client) {
-  console.log("getting new token need to fix this");
-  // const authUrl = oAuth2Client.generateAuthUrl({
-  //   access_type: 'offline',
-  //   scope: SCOPES,
-  // });
-  // console.log('Authorize this app by visiting this url:', authUrl);
-  // const rl = readline.createInterface({
-  //   input: process.stdin,
-  //   output: process.stdout,
-  // });
-  // rl.question('Enter the code from that page here: ', (code) => {
-  //   rl.close();
-  //   oAuth2Client.getToken(code, (err, token) => {
-  //     if (err) return console.error('Error while trying to retrieve access token', err);
-  //     oAuth2Client.setCredentials(token);
-  //     // Store the token to disk for later program executions
-  //     fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-  //       if (err) return console.error(err);
-  //       console.log('Token stored to', TOKEN_PATH);
-  //     });
-  //     // callback(oAuth2Client);
-  //   });
-  // });
-}
-
-// get majors demo
-app.get('/api/majors', async (req, res) => {
-  console.log("get request");
-  console.log(auth);
-  const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const rows = res.data.values;
-    if (rows.length) {
-      console.log('Name, Major:');
-      // Print columns A and E, which correspond to indices 0 and 4.
-      rows.map((row) => {
-        console.log(`${row[0]}, ${row[1]}`);
-        // console.log(`${row[0]}, ${row[4]}`);
-      });
-    } else {
-      console.log('No data found.');
-    }
-  });
-})
-
 // get broadcast reach tracker
 app.get('/api/broadcast-reach', async (req, res) => {
   const sheets = google.sheets({version: 'v4', auth});
@@ -123,73 +68,55 @@ app.get('/api/broadcast-reach', async (req, res) => {
   });
 })
 
-// get broadcast reach tracker
-app.get('/api/ebook', async (req, res) => {
-  const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.batchGet({
-    spreadsheetId: '1gkD5zCiyiIomR3_TkkoVtgnVmBGCXhphGKn7uy_Zz5I',
-    ranges: ['Worldreader/Amazon!H2:H', 'Worldreader/Amazon!I2:I', 'Worldreader/Amazon!N2:N'],
-    // range: 'Worldreader/Amazon!M2:M',
-  }, (err, sheetsRes) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    console.log("get ebooks");
-    var columns = [];
-    let i;
-    for (i = 0; i < sheetsRes.data.valueRanges.length; i++) { 
-      columns.push(sheetsRes.data.valueRanges[i].values);
-    }
+// app.route('/api/ebooks/revenue/:month')
+//   .get(function(req, res, next) {
+//     db.query(
+//       "SELECT * FROM `ebooks` WHERE month = ?", req.params.month,
+//       function(error, results, fields) {
+//         if (error) throw error;
+//         res.json(results);
+//       }
+//     );
+//   });
 
-    var monthYear = columns[0].map((e, i) => `${e}-${columns[1][i]}`);
-    var uniqueMonths = Array.from(new Set(monthYear))
-    // console.log(uniqueMonths);
-
-    let j;
-    var netPerMonth = {};
-    for (j = 0; j < uniqueMonths.length; j++) {
-      netPerMonth[uniqueMonths[j]] = 0;
-    }
-
-    let k;
-    for (k = 0; k < columns[0].length; k++) {
-      netPerMonth[monthYear[k]] = netPerMonth[monthYear[k]] + parseInt(columns[2][k][0].replace(/[$.]/g, ""), 10) / 100; 
-    }
-
-    var netPerMonthArr = [];
-    for (var key in netPerMonth) {
-      netPerMonthArr.push([key, netPerMonth[key].toFixed(2)]);
-    }
-    console.log(netPerMonthArr);
-    res.send(netPerMonthArr);
-    // var rows = [];
-    // let j;
-    // console.log(columns[0].length);
-    // for (j = 0; j < columns[0].length; j++) {
-    //   rows.push([columns[0][j], columns[1][j]]);
-    // }
-    // const rows = sheetsRes.data.values;
-    // if (rows.length) {
-    //   console.log(rows.length, ' rows found.');
-    //   console.log(rows)
-    // } else {
-    //   console.log('No data found.');
-    // }
-    // res.send(rows);
+app.route('/api/revenue/ebooks')
+  .get(function(req, res, next) {
+    db.query(
+      "SELECT month, year, SUM(net) AS revenue FROM `ebooks` GROUP BY month, year",
+      function(error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.json(results);
+      }
+    );
   });
-})
 
-// Get scores
-app.get('/api/scores', async (req, res) => {
-  const query = await db.query('SELECT * FROM players ORDER BY name;');
-  res.send(query.rows);
-});
+app.route('/api/revenue/albums')
+  .get(function(req, res, next) {
+    db.query(
+      "SELECT month, year, SUM(total_earned) AS revenue FROM `albums` GROUP BY month, year",
+      function(error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+      }
+    );
+  });
 
-// For any request that doesn't match, we'll send the index.html file from the client. This is used for all of our React code.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+app.route('/api/revenue/ios')
+  .get(function(req, res, next) {
+    db.query(
+      "SELECT month, year, SUM(net) AS reveunue FROM `ios_apps` GROUP BY month, year",
+      function(error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+      }
+    );
+  });
 
-// Set localhost port for server
-const port = process.env.PORT || 5000;
-app.listen(port);
 
-console.log(`ubongo dashboard listening on ${port}`);
+
+app.get('/status', (req, res) => res.send('Working!'));
+
+// Port 8080 for Google App Engine
+app.set('port', process.env.PORT || 8080);
+app.listen(8080);
